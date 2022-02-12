cc7f7109842708b6efb3f71cbf42735db65bcf67
adya333 <90618516+adya333@users.noreply.github.com>
2022-02-13 02:39:11 +0530

    Update Week1_Q5.c

--- Week1_Q5.c
+++ Week1_Q5.c
@@ -2,11 +2,11 @@
 int main(){
        float rad,dia,circ,area;
        printf("enter the radius:");
-       scanf("%f",&rad);
+       scanf("%0.2f",&rad);
        dia=2*rad;circ=2*3.1416*rad;area=3.1416*rad*rad;
-       printf("diameter:%f\n",dia);
-       printf("area:%f\n",area);
-       printf("Circumference:%f\n",circ);
+       printf("Diameter of the circle:%0.2f\n",dia);
+       printf("Area of the circle:%0.2f\n",area);
+       printf("Circumference of the circle:%0.2f\n",circ);
          
 return 0;
 }
