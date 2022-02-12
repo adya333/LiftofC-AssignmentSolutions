49536bc07b6b97082bf76bb2abfeb66cfdad57bd
adya333 <90618516+adya333@users.noreply.github.com>
2022-02-13 02:16:56 +0530

    Update Week1_Q1.c

--- Week1_Q1.c
+++ Week1_Q1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    char name [20];
-    char red [3];
-    char branch[20];
-    char hobbies[20];
+    char name [50];
+    int red ;
+    char branch[50];
+    char hobbies[50];
     printf("enter the info\n");
     gets(name);
-    scanf("%s", &red);
+    scanf("%d", &red);
     scanf("%s", &branch);
     scanf("%s", &hobbies);
-    printf("%s\n",name);
-    printf("%s\n",red);
+    puts(name);
+    printf("%d\n",red);
     printf("%s\n",branch);
     printf("%s\n",hobbies);
     return 0;
